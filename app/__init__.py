35f6603fd920c9355ff4196470597e7deaa7cb83
Samir Lilienfeld <samir@Samirs-MacBook-Air.local>
2024-10-09 18:57:07 -0400

    completed

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from app.shipping_form import ShippingForm
+
+from flask_migrate import Migrate
+
+from app.config import Config
+from app.models import Package, db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+@app.route('/')
+def root_endpoint():
+    return "Package Tracker"
+
+@app.route('/new_package', methods=["GET", "POST"])
+def form():
+    form = ShippingForm()
+    return render_template("shipping_request.html", form=form)
---
+++ app/shipping_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+from map.map import map
+
+cities = [(city, city) for city in map.keys()]
+
+
+class ShippingForm(FlaskForm):
+    sender_name = StringField('Sender', validators=[DataRequired()])
+    recipient_name = StringField('Recipient', validators=[DataRequired()])
+    origin = SelectField('Origin', choices=cities, validators=[DataRequired()])
+    destination = SelectField('Destination', choices=cities, validators=[DataRequired()])
+
+    submit = SubmitField("Ship it!")
